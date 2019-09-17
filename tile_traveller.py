66188c52c48434664aa33836d1940b1a66c13395
Eva Sól Pétursdóttir <evasolpetursdottir@Evas-MacBook-Air-2.local>
2019-09-17 09:23:00 +0000

    Algorithm for tile_traveller

---
+++ tile_traveller.py
@@ -0,0 +1,6 @@
+# Step 1: Find valid moves for the first tile
+# Step 2: Get a move from the user
+# Step 3: If the move from the user is valid then move, othewise let him know that it is unvalid and get a new move from the user
+# Step 4: Find valid moves for the next tile
+# Step 5: Do step 2-4 until the user is in tile 3.1
+# Step 6: Victory!
