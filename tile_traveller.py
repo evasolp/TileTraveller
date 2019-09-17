2aa38b408fd0f46f76f6371657c37b0c5242e07f
hallaj19 <hallaj19@ru.is>
2019-09-17 09:55:51 +0000

    skeleton for tile_traveller.py

--- tile_traveller.py
+++ tile_traveller.py
@@ -4,3 +4,40 @@
 # Step 4: Find valid moves for the next tile
 # Step 5: Do step 2-4 until the user is in tile 3.1
 # Step 6: Victory!
+
+
+
+def direction_from_user():
+    '''Gets a N, W, E or S from the user, (depending on where the user wants/can move) and returns it '''  
+
+
+def valid_moves():
+    '''Checks if input from user is valid, and returns True or None''' 
+
+
+def move():
+    '''Takes the current tile and move direction in and returns the new tile''' 
+    return
+
+
+start_position_float = 1.1
+
+
+direction = direction_from_user()
+
+
+position = start_position_float
+
+while position != 3.1:
+    if valid_moves(direction) == True:
+        position = move()
+        #látum position og direction í move fallið
+        direction = direction_from_user()
+
+
+    else:
+        print("Not a valid direction!")
+        direction = direction_from_user()
+
+else:
+    print("Victory!")
